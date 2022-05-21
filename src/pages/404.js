3e1bf117ab1a636df2a6e3339e088a0a514d56a2
Viviana Duarte <yvduartef@gmail.com>
2022-05-20 22:37:50 -0500

    first version with error404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './style.css';
+import { Link } from 'react-router-dom';
 
 function Error4(){
     return (
         <div class="error404">
+            <Link to="/home" className='error item-nav'><i class="fa-solid fa-user"></i>Volver al Home</Link>
             <h2>Error 404: La página no existe</h2>
             <img src="https://cdn3.iconfinder.com/data/icons/lightly-icons/30/652868-surprised-480.png" alt='icono'></img>
         </div>
